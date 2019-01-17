77877d15c21cfd17e8650d70b51248e30b4b5c41
Diaz Martin <frdz@gft.com>
2019-01-17 16:12:41 +0100

    Some styles improvements and home link

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -5,11 +5,11 @@ export function RentalCard(props) {
   const { rental } = props;
   return (
     <div className='col-md-3 col-xs-6'>
-      <Link to={`/rentals/${rental.id}`}>
+      <Link className='rental-detail-link' to={`/rentals/${rental.id}`}>
         <div className='card bwm-card'>
-          <img className='card-img-top' src={rental.image} alt=''></img>
+          <img className='card-img-top' src={rental.image} alt='rental main image'></img>
           <div className='card-block'>
-            <h6 className='card-subtitle'>{rental.hared ? 'shared' : 'whole'} {rental.category} &#183; {rental.city}</h6>
+            <h6 className={`card-subtitle ${rental.category}`}>{rental.hared ? 'shared' : 'whole'} {rental.category} &#183; {rental.city}</h6>
             <h4 className='card-title'>{rental.title}</h4>
             <p className='card-text'>${rental.dailyRate} per Night &#183; Free Cancelation</p>
           </div>
