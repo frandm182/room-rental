0f1c1d11efa9ecea42744964f184062a4dbfe0e3
Diaz Martin <frdz@gft.com>
2019-01-25 08:30:06 +0100

    Added findAll and findById service in rental routes

--- server/routes/rental.js
+++ server/routes/rental.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const Rental = require('../models/rental');
 
 router.get('', (req, res) => {
-  res.json({ 'ok': true });
+  Rental.find({}, (err, foundRentals) => {
+    res.json(foundRentals);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Rental.findById(id, (err, foundRental) => {
+    console.log(id)
+    res.json(foundRental);
+  });
 });
 
 module.exports = router;
